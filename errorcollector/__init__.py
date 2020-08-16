dd7476021b82d25d98166dc2acb0fdf415670138
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-08-16 12:32:43 +0900

    Define API

--- errorcollector/__init__.py
+++ errorcollector/__init__.py
@@ -1,2 +1,10 @@
 """Collects raised error during with statement."""
+from typing import List
+
+from errorcollector.error_collector import *  # noqa
+
 __version__ = "0.1.0"
+
+__all__: List[str] = []
+# pylint: disable=undefined-variable
+__all__ += error_collector.__all__  # type: ignore # noqa
