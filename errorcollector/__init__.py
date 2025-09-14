f2da75954300253d32a090e7b21c4eda1f3dc5ef
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-09-14 13:07:22 +0900

    Bump version: 0.1.0 → 1.0.1

--- errorcollector/__init__.py
+++ errorcollector/__init__.py
@@ -2,7 +2,7 @@
 
 from errorcollector.error_collector import *  # noqa: F403
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = []
 __all__ += error_collector.__all__  # type:ignore[name-defined] # noqa: F405 pylint: disable=undefined-variable
